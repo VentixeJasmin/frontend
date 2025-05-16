45a91005bccf001a6297294bd207454644ae7d25
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-05-16 13:30:40 +0200

    Start page (unauth) designed.

--- src/assets/layouts/CenterLayout.jsx
+++ src/assets/layouts/CenterLayout.jsx
@@ -6,9 +6,25 @@ const centerLayout = () => {
   return (
     <>
       <div className="center-wrapper">
-        <Outlet />
-        <Footer />
+        <div className="headline-container">
+          <div className="headline">
+            <img className="headline-logo" src="public\images\logo-icon.svg"></img>
+            <h1>Ventixe</h1>
+          </div>
+          <div className="teaser">
+            <h2>It's your party - manage everything in one place.</h2>
+          </div>
+          <div className="button-container">
+            <button className="btn btn-pink btn-large">Sign In</button>
+            <button className="btn btn-pink btn-large">Sign Up</button>
+          </div>
+          
+        </div>
+        <div className="photo-byline">
+          <p>Photo Credit: Josh Sorenson/Pexels</p>
+        </div>
       </div>
+      
     </>
   )
 }
