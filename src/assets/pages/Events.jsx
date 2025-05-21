9b19c28efd207a90ba7789dcd178a458c07ab1f1
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-05-21 10:45:36 +0200

    Commit 7: Dashboard and Events can fetch events from API and display them as cards. Dashboard is still looking good responsive.

--- src/assets/pages/Events.jsx
+++ src/assets/pages/Events.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import EventCard from '../components/EventCard'
+
 
 const Events = () => {
- 
+  const [events, setEvents] = useState ([])
+
+  const url = "https://localhost:7234/api/Events/"
+
+  const getEvents = async () => {
+    const res = await fetch (url)
+
+    if (res.ok) {
+      const data = await res.json() 
+      console.log(data)
+      setEvents(data)
+    }
+  }
+
+  useEffect(() => {
+    getEvents()
+  }, [])
+
   
   return (
-    <div>Events</div>
+    <div>
+      <h2>Events</h2>
+      <div className="events-container">
+        {
+          events.map(event => (
+            <EventCard key={event.id} event={event} />
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
