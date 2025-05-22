04ad337d78255b242b8d7160a44a31e8c1b09b6d
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-05-22 15:27:46 +0200

    Commit 9: Location info added to EventCard, with fetch from VenueService Api.

--- src/assets/components/EventCard.jsx
+++ src/assets/components/EventCard.jsx
@@ -6,6 +6,25 @@ import { Link } from 'react-router-dom';
 const EventCard = ({event}) => {
     //Got help from Claude AI with the following breaking down of the date:
     const eventDate = new Date(event.date); 
+    const venueId = event.venueId
+
+     const [venue, setVenue] = useState ([])
+      
+        const venueUrl = "https://localhost:7095/api/Venues/"
+
+    
+        const getVenue = async () => {
+          const res = await fetch (venueUrl + venueId)
+      
+          if (res.ok) {
+            const data = await res.json() 
+            setVenue(data)
+          }
+        }
+      
+        useEffect(() => {
+          getVenue()
+        }, [])
 
     const formattedDate = eventDate.toLocaleDateString('en-US', {
         year: 'numeric',
@@ -26,7 +45,7 @@ const EventCard = ({event}) => {
         </div>
         <div className="event-card-info">
         <p className="title-text semi-bold event-title-text">{event.title}</p>
-        <p className="title-text light location-text">Sunset</p>
+        <p className="title-text light location-text">{venue.title}, {venue.city}</p>
         <p className="title-text description-text">{event.description}</p>
         <div className="detail-info">
             <div className="horizontal">
