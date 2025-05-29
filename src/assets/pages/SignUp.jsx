adfa3a4363fe71e183c38b57a5e4a6b60dadd46c
Jasmin de Freitas <jasmindefreitas@hotmail.com>
2025-05-29 11:52:57 +0200

    Error in api url corrected.

--- src/assets/pages/SignUp.jsx
+++ src/assets/pages/SignUp.jsx
@@ -25,7 +25,7 @@ const SignUp = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://authservice-jdf-bvbxf7b5bqdndqhy.swedencentral-01.azurewebsites.net/auth/signup', {
+      const response = await fetch('https://authservice-jdf-bvbxf7b5bqdndqhy.swedencentral-01.azurewebsites.net/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
